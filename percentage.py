cff50fbafd46e973ad823adb666933427a255ce5
Corentin MERCIER <corentin.mercier@epita.fr>
2018-02-18 21:48:14 +0800

    g-dax added

--- percentage.py
+++ percentage.py
@@ -9,8 +9,24 @@ import requests
 import json
 ################################################################################
 
+#################
+#### COINONE ####
+#################
+
 req = requests.get("https://api.coinone.co.kr/ticker/")
-print(req.status_code)
+# print(req.status_code)
 btc = json.loads(req.text)
 
+print("Coinone:")
 print(btc['last'])
+
+###############
+#### G-DAX ####
+###############
+
+req = requests.get("https://api.gdax.com/products/BTC-EUR/ticker")
+# print(req.status_code)
+btc = json.loads(req.text)
+
+print("G-DAX:")
+print(btc['ask'])
