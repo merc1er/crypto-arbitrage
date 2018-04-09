8812fdd0b3977c270041a36dd224f117f4c161b0
Corentin MERCIER <corentin.mercier@epita.fr>
2018-04-10 03:05:54 +0800

    fixed empty prices file

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 while true
 do
-python3 percentage.py btc > prices
-python3 percentage.py eth >> prices
-python3 percentage.py bch >> prices
-# python3 percentage.py ltc >> prices
-sleep 30
+  # writing on .new file so it doesn't remove altcoins while waiting for the
+  # request to come
+  python3 percentage.py btc > prices.new
+  python3 percentage.py eth >> prices.new
+  python3 percentage.py bch >> prices.new
+  # python3 percentage.py ltc >> prices
+  cat prices.new > prices
+  sleep 30
 done;
