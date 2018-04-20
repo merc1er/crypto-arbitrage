d5857686e28900c30a919ecf96ff7e6c50a76e74
Corentin MERCIER <corentin.mercier@epita.fr>
2018-04-20 10:02:43 +0800

    html activated

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ while true
 do
   # writing on .new file so it doesn't remove altcoins while waiting for the
   # request to come
-  python3 percentage.py btc > prices.new
-  python3 percentage.py eth >> prices.new
-  python3 percentage.py bch >> prices.new
-  python3 percentage.py ltc >> prices.new
+  python3 percentage.py btc --html > prices.new
+  python3 percentage.py eth --html >> prices.new
+  python3 percentage.py bch --html >> prices.new
+  python3 percentage.py ltc --html >> prices.new
   cat prices.new > prices
   sleep 30
 done;
