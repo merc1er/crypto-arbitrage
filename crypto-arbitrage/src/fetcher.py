bca8007dac5e7669d3d3a3bac9d29ae89fae1cda
Corentin Mercier <corentin@mercier.link>
2020-01-31 02:00:29 +0700

    Replace herrorHandler by raise_for_status()
    Built-in `requests` method

--- crypto-arbitrage/src/fetcher.py
+++ crypto-arbitrage/src/fetcher.py
@@ -9,59 +9,48 @@ import requests
 from .secret import FIXER_API_KEY
 
 
-def errorHandler(errors):
-    print('Error: ' + errors)
-    sys.exit()
-
-
-def krwCalc():
+def krw_rate():
     """ Converts 1 EUR to KRW"""
     if FIXER_API_KEY == '':
-        errorHandler('Input your fixer.io API key in secrets.py')
-    try:
-        endpoint = 'http://data.fixer.io/api/latest?access_key='
-        req = requests.get(endpoint + FIXER_API_KEY)
-        krw_string = req.json()['rates']['KRW']
-        krw = float(krw_string)
-        return krw
-    except Exception:
-        errorHandler("Couldn't fetch from fixer.io")
+        print('Input your fixer.io API key in secrets.py')
+        sys.exit()
+
+    endpoint = 'http://data.fixer.io/api/latest?access_key='
+    req = requests.get(endpoint + FIXER_API_KEY)
+    req.raise_for_status()
+    krw_string = req.json()['rates']['KRW']
+    krw = float(krw_string)
+    return krw
 
 
 def korbit(currency_in):
     """ Returns the value of 1 currency_in according to korbit """
-    try:
-        korbit_endpoint = 'https://api.korbit.co.kr/v1/ticker?currency_pair='
-        req = requests.get(korbit_endpoint + currency_in.lower() + "_krw")
-        btc_korbit = req.json()
-        price = float(btc_korbit['last']) / krwCalc()
-        return price
-    except Exception:
-        errorHandler('Could not fetch from korbit')
+    korbit_endpoint = 'https://api.korbit.co.kr/v1/ticker?currency_pair='
+    req = requests.get(korbit_endpoint + currency_in.lower() + "_krw")
+    req.raise_for_status()
+    btc_korbit = req.json()
+    price = float(btc_korbit['last']) / krw_rate()
+    return price
 
 
 def coinone(currency_in):
     """ Returns the value of 1 currency_in according to coinone """
-    try:
-        req = requests.get(url="https://api.coinone.co.kr/ticker/",
-                           params={"currency": currency_in})
-        btc_coinone = req.json()
-        price = float(btc_coinone['last']) / krwCalc()
-        return price
-    except Exception:
-        errorHandler('Could not fetch from coinone')
+    req = requests.get(url="https://api.coinone.co.kr/ticker/",
+                       params={"currency": currency_in})
+    req.raise_for_status()
+    btc_coinone = req.json()
+    price = float(btc_coinone['last']) / krw_rate()
+    return price
 
 
 def gdax(currency_in):
     """ Returns the value of 1 currency_in according to Coinbase Pro """
-    try:
-        req = requests.get("https://api.pro.coinbase.com/products/" +
-                           currency_in.upper() + "-EUR/ticker")
-        btc_gdax = req.json()
-        price = float(btc_gdax['ask'])
-        return price
-    except Exception:
-        errorHandler('Could not fetch from Coinbase Pro')
+    req = requests.get("https://api.pro.coinbase.com/products/" +
+                       currency_in.upper() + "-EUR/ticker")
+    req.raise_for_status()
+    btc_gdax = req.json()
+    price = float(btc_gdax['ask'])
+    return price
 
 
 def bittrex(currency_in):
@@ -75,25 +64,24 @@ def bittrex(currency_in):
 
 
 def cryptonator(currency_in, market):
-    try:
-        req = requests.get("https://api.cryptonator.com/api/full/" +
-                           currency_in.lower() + "-eur")
-        callback = req.json()
-        markets = callback['ticker']['markets']
-        if market == 'bitfinex':
-            return markets[0]['price']
-        if market == 'cexio':
-            return markets[1]['price']
-        if market == 'exmo':
-            return markets[2]['price']
-        if market == 'kraken':
-            return markets[3]['price']
-        if market == 'livecoin':
-            return markets[4]['price']
-        if market == 'wexnz':
-            return markets[5]['price']
-    except Exception:
-        errorHandler('Could not fetch from cryptonator')
+    req = requests.get("https://api.cryptonator.com/api/full/" +
+                       currency_in.lower() + "-eur")
+    req.raise_for_status()
+    callback = req.json()
+    markets = callback['ticker']['markets']
+    if market == 'bitfinex':
+        return markets[0]['price']
+    if market == 'cexio':
+        return markets[1]['price']
+    if market == 'exmo':
+        return markets[2]['price']
+    if market == 'kraken':
+        return markets[3]['price']
+    if market == 'livecoin':
+        return markets[4]['price']
+    if market == 'wexnz':
+        return markets[5]['price']
+    return False
 
 ###############################################################################
 #
