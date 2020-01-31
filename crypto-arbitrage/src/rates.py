70abbd0b09aae85be3b428530736d77a9a1013b3
Corentin Mercier <corentin@mercier.link>
2020-01-31 12:04:17 +0700

    Replaced fixer.io with exchangeratesapi.io
    exchangeratesapi.io is free and open source.
    
    My fixer.io api key has been voided.

--- crypto-arbitrage/src/rates.py
+++ crypto-arbitrage/src/rates.py
@@ -1,17 +1,11 @@
-import sys
 import requests
-from .secret import FIXER_API_KEY
 
 
 def krw_rate():
     """ Converts 1 EUR to KRW"""
-    if FIXER_API_KEY == '':
-        print('Input your fixer.io API key in secrets.py')
-        sys.exit()
-
-    endpoint = 'http://data.fixer.io/api/latest?access_key='
-    req = requests.get(endpoint + FIXER_API_KEY)
-    req.raise_for_status()
-    krw_string = req.json()['rates']['KRW']
+    endpoint = 'https://api.exchangeratesapi.io/latest'
+    r = requests.get(endpoint)
+    r.raise_for_status()
+    krw_string = r.json()['rates']['KRW']
     krw = float(krw_string)
     return krw
